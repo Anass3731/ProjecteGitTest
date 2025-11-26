05f3e1081b9a5973989d5d12aa60c39b7021bf7b
Anass <aoussar371@gmail.com>
2025-11-26 13:54:19 +0100

    neeew

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hola, projecte Git amb VS Code!");
+        Saludo();
+    }
+    public static void Saludo(){
+        System.out.println("Holaaa Soy Anass que tal?");
     }
 }
